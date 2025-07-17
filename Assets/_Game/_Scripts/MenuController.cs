73599d03b1bf31a297091e4ffccadbd082fc4cd5
akeel-h <akilabdul1912@gmail.com>
2025-07-17 17:36:13 +0800

    Added Inventory UI
    Added Inventory UI (press Tab)

---
+++ Assets/_Game/_Scripts/MenuController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    void Start()
+    {
+        menuCanvas.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+        }
+    }
+}
